a24bb1207f692d0b3ee2dd08e94124ebbe2d3945
KHOVKhunly <eloonmacor@gmail.com>
2025-09-02 10:48:33 +0700

    Homework-3-Individual

---
+++ Module_2-Practice/Errors/T08_Errors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_2_Practice.Errors
+{
+	internal class T08_Errors
+	{
+		static void Main(string[] args)
+		{
+			//int num = Console.ReadLine(); // ❌ ERROR: cannot assign string to int
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            //if num > 0 // ❌ ERROR: missing parentheses
+            if (num > 0)
+
+                Console.WriteLine("Positive");
+
+			//string result = num % 2 = 0 ? "Even" : "Odd"; // ❌ ERROR: wrong operator
+            string result = num % 2 == 0 ? "Even" : "Odd";
+
+            Console.WriteLine(result);
+		}
+	}
+}
---
+++ Module_2-Practice/Exercises/T10_Exercise.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_2_Practice.Exercises
+{
+	internal class T10_Exercise
+	{
+		static void Main(string[] args)
+		{
+			// TODO:
+			// 1. Declare 2 variables
+			// 2. Use nameof to print their names
+
+			int num1 = 15;
+			double num2 = 10.5;
+
+            Console.WriteLine($"{nameof(num1)} = {num1}");
+            Console.WriteLine($"{nameof(num2)} = {num2}");
+        }
+	}
+}
