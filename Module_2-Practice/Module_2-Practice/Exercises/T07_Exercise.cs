27db05b38ca5db9ca408678b0da9ef5846afa6f6
RCM-Khunly Khov <khunly.khov@eastmfi.com>
2025-09-04 13:43:28 +0700

    Homework-3-Individual

---
+++ Module_2-Practice/Module_2-Practice/Exercises/T07_Exercise.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_2_Practice.Exercises
+{
+	internal class T07_Exercise
+	{
+		static void Main(string[] args)
+		{
+            // TODO:
+            // 1. Declare two integers
+            // 2. Use arithmetic, relational, and logical operators
+            // 3. Print all results
+
+            int a = 10, b = 0;
+
+            if (b != 0)
+                Console.WriteLine("Division: " + (a / b));
+            else
+                Console.WriteLine("Division: cannot divide by zero");
+
+            bool x = true, y = false;
+           
+            Console.WriteLine("x & y: " + (x & y));   // bitwise AND
+            Console.WriteLine("x && y: " + (x && y)); // logical AND
+
+            string? str = null;
+            Console.WriteLine("Length: " + (str?.Length ?? 0));
+        }
+	}
+}
---
+++ Module_2-Practice/Module_2-Practice/Exercises/T12_Exercise.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_2_Practice.Exercises
+{
+	internal class T12_Exercise
+	{
+		internal class T12_Exercises
+		{
+			static void Main(string[] args)
+			{
+                // TODO:
+                // 1. Create string with your name
+                // 2. Print length, uppercase, substring
+                // 3. Replace part of string
+
+                // 1. Create string with your name
+                string name = "Khov Khunly";
+
+                // 2. Print length, uppercase, substring
+                Console.WriteLine("Original: " + name);
+                Console.WriteLine("Length: " + name.Length);          
+                Console.WriteLine("Uppercase: " + name.ToUpper());    
+                Console.WriteLine("Substring (all 11 chars): " + name.Substring(0, 11)); 
+
+                // 3. Replace part of string
+                string replaced = name.Replace("Khov Khunly", "Khunly Khov");
+                Console.WriteLine("Replaced: " + replaced);
+
+            }
+        }
+	}
+}
