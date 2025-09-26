13ffeb706f15f8edd2fb13aadf170975da159e75
KHOVKhunly <eloonmacor@gmail.com>
2025-09-26 08:38:12 +0700

    Patern_26

---
+++ Pattern_26_App/Pattern_26_App/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Pattern_26_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Pattern_26_App ===");
+            int max = 7;
+
+            // Top half of the diamond
+            for (int i = 1; i <= max; i++)
+            {
+                Console.Write(new string(' ', max - i));
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            // Bottom half of the diamond
+            for (int i = max - 1; i >= 1; i--)
+            {
+                Console.Write(new string(' ', max - i));
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
