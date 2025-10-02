192d712d83762f3b5c2e6d53c06d1df2265201fb
KHOVKhunly <eloonmacor@gmail.com>
2025-10-02 14:04:10 +0700

    Homework_Individual_DeadLine_Oct4,25

---
+++ HomeWorkIndividual_Calculation/HomeWorkIndividual_Calculation/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace HomeWorkIndividual_Calculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("==== Calculator Menu ====");
+                Console.WriteLine("1. Addition");
+                Console.WriteLine("2. Subtraction");
+                Console.WriteLine("3. Multiplication");
+                Console.WriteLine("4. Division");
+                Console.WriteLine("5. Exit");
+ 
+            while (true)
+            {
+                Console.Write("Enter Your Choice (1-5): ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                decimal num1 = 0, num2 = 0;
+
+                if (num >= 1 && num <= 4)
+                {
+                    Console.Write("Enter Your First Number: ");
+                    num1 = Convert.ToDecimal(Console.ReadLine());
+
+                    Console.Write("Enter Your Second Number: ");
+                    num2 = Convert.ToDecimal(Console.ReadLine());
+                }
+
+                if (num == 1)
+                {
+                    Console.WriteLine($"Addition Result is: {num1 + num2}");
+                }
+                else if (num == 2)
+                {
+                    Console.WriteLine($"Subtraction Result is: {num1 - num2}");
+                }
+                else if (num == 3)
+                {
+                    Console.WriteLine($"Multiplication Result is: {num1 * num2}");
+                }
+                else if (num == 4)
+                {
+                    if (num2 != 0)
+                        Console.WriteLine($"Division Result is: {num1 / num2}");
+                    else
+                        Console.WriteLine("Division by zero is not allowed.");
+                }
+                else if (num == 5)
+                {
+                    Console.Write("Are you sure, you want to exit? (Y/N): ");
+                    string confirm = Console.ReadLine();
+                    if (confirm?.ToUpper() == "Y")
+                    {
+                        Console.WriteLine("Exiting program");
+                        break; 
+                    }
+                    else
+                    {
+                        Console.WriteLine("Back to menu\n");
+                        continue; 
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Number. Please choose between 1–5.");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ M2_Loop_Practices_Problem/M2_Loop_Practices_Problem/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace M2_Loop_Practices_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Console.WriteLine("==== Welcome to loop ===");
+                Console.WriteLine("1. Home");
+                Console.WriteLine("2. About us");
+                Console.WriteLine("3. Contact us");
+                Console.WriteLine("4. Privacy");
+                Console.WriteLine("5. Exit");
+
+            while (true)
+            {
+                Console.Write("Please choose Menue 1-5: Option: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine("Welcome to Home Page");
+                        break;
+                    case 2:
+                        Console.WriteLine("Welcome to About us");
+                        break;
+                    case 3:
+                        Console.WriteLine("Welcome to Contact us");
+                        break;
+                    case 4:
+                        Console.WriteLine("Welcome to Privacy Page");
+                        break;
+                    case 5:
+                        Console.Write("Are you sure to Exit now? (Y/N): ");
+                        string confirm = Console.ReadLine(); 
+
+                        if (confirm.ToUpper() == "Y")
+                        {
+                            Console.WriteLine("Existing Program");
+                            Environment.Exit(0); 
+                        }
+                        else
+                        {
+                            Console.WriteLine("Back to Menu");
+                            break; 
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input ");
+                        break;
+                }
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
