b330f7f4decfe2cbfed53bfb9bf1fd50533f1187
KHOVKhunly <eloonmacor@gmail.com>
2025-10-21 12:17:12 +0700

    Homework on Oct 18, 2025

---
+++ ClassDiagram_Draw1/ClassDiagram_Draw1/Roles.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassDiagram_Draw1
+{
+    public class Roles
+    {
+        public int Id
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Role
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Users UsersRoles
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void addRole()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void deleteRole()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void editRole()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void viewRole()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ ClassDiagram_Draw1/ClassDiagram_Draw1/Users.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassDiagram_Draw1
+{
+    public class Users
+    {
+        public int Id
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Password
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Role
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Username
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void addUser()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void deleteUser()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void editUser()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Login()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void viewUser()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
